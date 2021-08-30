f330a15e1e76c6ee49661a2fee4b873c8075d547
vishnuvardhan860 <vishnuvardhan860@gmail.com>
2021-08-30 19:54:36 +0530

    Added: View Contact By City

--- AddressBook.js
+++ AddressBook.js
@@ -1,46 +1,6 @@
-// Search contact by city.
-
+//View Contact By city
 const prompt = require('prompt-sync')();
-
-let nameRegex = RegExp("^[A-Z]{1}[a-z]{2,}$");
-let addressRegex = RegExp("^[A-Za-z]{4,}$");
-let cityStateRegex = RegExp("^[A-Za-z]{4,}$");
-let zipRegex = RegExp("^[1-9]{1}[0-9]{2}[ ]?[0-9]{3}$");
-let phoneNumberRegex = RegExp("^[1-9]{1}[0-9]{9}$");
-let emailRegex = RegExp("^([a-z]+)([0-9])*([_+-.]{1}[a-z0-9]+)*(@)([a-z0-9]+)[.]([a-z]{2,})([.][a-z]{2}){0,1}$");
-class Contact {
-    constructor(...params) {
-        if (nameRegex.test(params[0]))
-            this.firstName = params[0];
-        else throw "Invalid First Name!!";
-        if (nameRegex.test(params[1]))
-            this.lastName = params[1];
-        else throw "Invalid Last Name!!";
-        if (addressRegex.test(params[2]))
-            this.address = params[2];
-        else throw "Invalid Address!!";
-        if (cityStateRegex.test(params[3]))
-            this.city = params[3];
-        else throw "Invalid City!!";
-        if (cityStateRegex.test(params[4]))
-            this.state = params[4];
-        else throw "Invalid State!!";
-        if (zipRegex.test(params[5]))
-            this.zip = params[5];
-        else throw "Invalid Zip!!";
-        if (phoneNumberRegex.test(params[6]))
-            this.phoneNumber = params[6];
-        else throw "Invalid Phone Number!!";
-        if (emailRegex.test(params[7]))
-            this.email = params[7];
-        else throw "Invalid Email!!";
-    }
-
-    toString() {
-        return "First Name : " + this.firstName + ", Last Name : " + this.lastName + ", Address : " + this.address + ", City : " + this.city + ", State : " + this.state + ", Zip : " + this.zip + ", Phone Number : " + this.phoneNumber + ", Email : " + this.email;
-    }
-}
-
+const Contact = require('./Contact.js')
 
 let addressBookArr = new Array();
 
@@ -66,8 +26,10 @@ let getContact = () => {
 let countContacts = () => addressBookArr.reduce((total, contact) => total + 1, 0);
 
 let viewContacts = () => {
+    console.log("Number of contacts in this addressbook : " + countContacts());
     addressBookArr.forEach(contact => console.log(contact.toString()));
 }
+
 
 let addContact = (contact) => {
     let index = getindexByName(contact.firstName, contact.lastName);
@@ -98,7 +60,7 @@ let editContact = () => {
 
 let deleteContact = () => {
     let frstName = prompt("Enter First Name : ");
-    let lstName = prompt("Enter Lastt Name : ");
+    let lstName = prompt("Enter Last Name : ");
     let index = getindexByName(frstName, lstName);
     if (index == -1)
         console.log("Could not find the contact!!")
@@ -141,4 +103,5 @@ do {
             break;
         default: console.log("Invalid Choice !!");
     }
+
 } while (choice != 7)
